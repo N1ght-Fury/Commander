48aedd7d776e7ff6d794e406399e22f9aab417b2
N1ght-Fury <44553433+N1ght-Fury@users.noreply.github.com>
2020-09-13 12:38:29 +0300

    No longer needed

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -1,10 +1,17 @@
 using System.Collections.Generic;
 using Commander.Models;
 
+//No longer needed
+
 namespace Commander.Data
 {
     public class MockCommanderRepo : ICommanderRepo
     {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
             var commands = new List<Command>
@@ -20,6 +27,11 @@ namespace Commander.Data
         public Command GetCommandById(int id)
         {
             return new Command { Id = 0, HowTo = "Test", Line = "Yay", Platform = "Pan" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
