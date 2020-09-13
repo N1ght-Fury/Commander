2c3fae3e50387e94544d17dd428a5bd310359db1
N1ght-Fury <44553433+N1ght-Fury@users.noreply.github.com>
2020-09-13 18:42:10 +0300

    Added required decorators

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -1,9 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Commander.Dtos
 {
     public class CommandCreateDto
     {
+        //Added required here, so when client does not supply any of the fields,
+        //instead of returning internal server error, we can return bad rerquest error
+        //and show client what they did wrong
+        [Required]
+        [MaxLength(250)]
         public string HowTo { get; set; }
+
+        [Required]
         public string Line { get; set; }
+
+        [Required]
         public string Platform { get; set; }
     }
 }
